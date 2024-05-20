96c948b4bec4407312fa4d6568475833d18afebc
Mathias Keulen <mathias.keulen@external.engie.com>
2024-05-20 14:59:36 +0200

    add square and refactor exports

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,14 @@
 // functions to be tested
 // can also be written on one line because it's a single return statement
-module.exports.add = (a, b) => {
+function add(a, b) {
   return a + b;
+}
+
+function square(x) {
+  x * x;
+}
+
+module.exports = {
+  add,
+  square,
 };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,10 +1,21 @@
 const utils = require("./utils");
 describe("utils", () => {
   it("should add two numbers", () => {
-    const res = utils.add("test", 10);
+    const res = utils.add(20, 10);
     switch (res) {
       case res !== 30:
         throw new Error(`Expected 30 but got ${res}`);
+      case typeof res !== "number":
+        throw new Error(`Expected a number but output is ${typeof res}`);
+      default:
+        break;
+    }
+  });
+  it("square add two numbers", () => {
+    const res = utils.square(10);
+    switch (res) {
+      case res !== 100:
+        throw new Error(`Expected 100 but got ${res}`);
       case typeof res !== "number":
         throw new Error(`Expected a number but output is ${typeof res}`);
       default:
